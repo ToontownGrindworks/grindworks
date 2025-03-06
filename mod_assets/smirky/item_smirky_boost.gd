66e218a66b4404f12599ab42e8c6deb0ff836838
Buckstrom <58726188+Buckstrom@users.noreply.github.com>
2025-03-05 17:02:54 -0800

    Smirky: Auto sound initialized (copy of Bessie Boost)

---
+++ mod_assets/smirky/item_smirky_boost.gd
@@ -0,0 +1,36 @@
+extends ItemScript
+
+const SOUND_GAGS := preload("res://objects/battle/battle_resources/gag_loadouts/gag_tracks/sound.tres")
+const AUTO_SOUND := preload("res://objects/battle/battle_resources/status_effects/resources/auto_sound.tres")
+
+var player: Player
+
+
+func on_collect(_item: Item, _object: Node3D) -> void:
+	var player: Player
+	if not Util.get_player():
+		player = await Util.s_player_assigned
+	else:
+		player = Util.get_player()
+	setup(player)
+
+func on_load(item: Item) -> void:
+	on_collect(item, null)
+
+func setup(_player: Player) -> void:
+	player = _player
+	BattleService.s_battle_started.connect(try_apply_sound)
+	BattleService.s_round_ended.connect(try_apply_sound)
+
+func try_apply_sound(manager: BattleManager) -> void:
+	# Pick a random cog in the battle and apply the auto-sound status onto them.
+	if manager.cogs:
+		var cog: Cog = RandomService.array_pick_random('true_random', manager.cogs)
+		var new_status := AUTO_SOUND.duplicate()
+		new_status.sound_gag = get_random_sound_resource()
+		new_status.target = cog
+		manager.add_status_effect(new_status)
+
+func get_random_sound_resource() -> GagSound:
+	var idx: int = RandomService.randi_range_channel('true_random', 0, player.stats.get_highest_gag_level() - 1)
+	return SOUND_GAGS.gags[idx].duplicate()
---
+++ objects/battle/battle_resources/status_effects/auto_sound.gd
@@ -0,0 +1,30 @@
+@tool
+extends StatusEffect
+class_name StatusAutoSound
+
+@export var sound_gag: GagSound
+
+func apply() -> void:
+	manager.s_round_started.connect(round_started)
+
+func cleanup() -> void:
+	if manager.s_round_started.is_connected(round_started):
+		manager.s_round_started.disconnect(round_started)
+
+func round_started(_actions: Array[BattleAction]) -> void:
+	var new_sound: GagSound = sound_gag.duplicate()
+	new_sound.targets = [target]
+	new_sound.user = Util.get_player()
+	#TODO: Make this into toon summons with sound
+	#new_sound.special_action_exclude = true
+	#new_sound.skip_button_movie = true
+	manager.inject_battle_action(new_sound, 0)
+
+func get_icon() -> Texture2D:
+	return sound_gag.icon
+
+func get_status_name() -> String:
+	return "Incoming Sound"
+
+func get_description() -> String:
+	return "Will be hit by %s\nDamage: %s" % [sound_gag.action_name, sound_gag.get_true_damage()]
