e900e5f869db086a1e51465971e9b4cb9eaa76fc
Buckstrom <58726188+Buckstrom@users.noreply.github.com>
2025-03-08 13:10:06 -0800

    sans: New interaction for max laff gain

--- mod_assets/sans/item_sans_judgement.gd
+++ mod_assets/sans/item_sans_judgement.gd
@@ -1,5 +1,8 @@
 extends ItemScript
 
+@export var defense_penalty := -0.05
+@export var max_laff_to_defense := 0.05
+
 func on_collect(_item: Item, _object: Node3D) -> void:
 	setup()
 
@@ -14,12 +17,20 @@ func setup() -> void:
 		player = Util.get_player()
 	
 	player.stats.hp_changed.connect(hp_changed)
+	player.stats.max_hp_changed.connect(hp_changed)
 	hp_changed(player.stats.hp)
 
 func hp_changed(hp: int) -> void:
 	if hp > 0:
 		return
-	Util.get_player().stats.defense -= 0.05
+	Util.get_player().stats.defense += defense_penalty
+
+func max_hp_changed(max_hp: int) -> void:
+	if max_hp > 1:
+		var diff = max_hp - 1
+		Util.get_player().stats.defense += max_laff_to_defense * diff
+		Util.get_player().stats.hp = 1
+		Util.get_player().stats.max_hp = 1
 
 func do_heal(amount: int) -> void:
 	Util.get_player().quick_heal(amount)
